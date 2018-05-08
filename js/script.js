a4cc11f173b23827df16604bc8ed9852d5c4f50d
Gigi Frias <gigi.frias@gmail.com>
2018-05-08 18:17:14 -0400

    Updating code and finding new data—need to combine data into one CSV/TSV

--- js/script.js
+++ js/script.js
@@ -7,382 +7,315 @@ var statesGlobal = [];
 var j = 0; // this is the index of which "type" of data we're looking at. Gross=0, PerCap=1
 var StandardPadding = 20;
 
-var w = parseInt(d3.select("#master_container").style("width"))
+var w = parseInt( d3.select( "#master_container" ).style( "width" ) )
 // var h = (AxisPaddingTop + StandardPadding/2 + ((totes)*2*r) + (totes*BubblePadding)+BubblePadding*1.5+20)
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = w - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 40
+  },
+  width = w - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select("#master_container").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select( "#master_container" ).append( "svg" )
+  .attr( "width", width + margin.left + margin.right )
+  .attr( "height", height + margin.top + margin.bottom )
+  .append( "g" )
+  .attr( "transform", "translate(" + margin.left + "," + margin.top + ")" );
 
-var parseDate = d3.time.format("%Y").parse;
+var parseDate = d3.time.format( "%Y" ).parse;
 // var parseDate2 = d3.time.format("%m/%d/%Y").parse;
 
 var x = d3.time.scale()
-    .range([0, (width)]);
+  .range( [ 0, ( width ) ] );
 
 var y = d3.scale.linear()
-    .range([height, 0]);
-
-// var y2 = d3.scale.linear()
-//     .range([height, 0]);    
+  .range( [ height, 0 ] );
 
 var color = d3.scale.category10();
-// var color2 = d3.scale.category10();
 
 var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");    
+  .scale( x )
+  .orient( "bottom" );
 
 var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .outerTickSize(0);   
+  .scale( y )
+  .orient( "left" )
+  .outerTickSize( 0 );
 
-// var yAxis2 = d3.svg.axis()
-//     .scale(y2)    
-//     .orient("right")
-//     .outerTickSize(0);       
+d3.tsv( "data/combined_hor.tsv", function ( error, data ) {
+  if ( error ) throw error
 
-d3.tsv("data/combined_hor.tsv", function(error, data) {
-  if (error) throw error;
-  // d3.tsv("data/barrels.tsv", function(error, barrels) {
-  //   if (error) throw error;
+  // ensure one tick per year based on data
+  xAxis.ticks( data.length );
 
-    // console.log(data)
-    // console.log(barrels)
+  color.domain( d3.keys( data[ 0 ] ).filter( function ( key ) {
+    return key !== "date" && key.slice( -2 ) !== "01";
+  } ) );
 
-    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date" && key.slice(-2) !== "01"; }));
-  
-    // color2.domain(d3.keys(barrels[0]).filter(function(key) { return key !== "date"}));
+  data.forEach( function ( d ) {
+    d.date = parseDate( d.date );
+  } );
 
+  // declare array variable for combined data
+  var comboValues = [];
 
-    data.forEach(function(d) {
-      d.date = parseDate(d.date);
-    });
+  var countries = color.domain().map( function ( name ) {
+    statesGlobal.push( name )
+    return {
+      name: name,
+      values: data.map( function ( d ) {
+        var f = +d[ name ];
+        comboValues.push( f ); // push values to combo array
 
-    // barrels.forEach(function(d){
-    //   d.date = parseDate2(d.date)
-    // })
+        // Loops and adds in other intensities!
+        for ( var k in d ) {
+          if ( k.slice( 0, -2 ) == name && k.slice( -2 ) == 01 ) {
+            var g = +d[ k ];
+            comboValues.push( g ); // push values to combo array
+          }
+        }
+        return {
+          date: d.date,
+          co2: f,
+          pcc: g
+        };
+      } )
+    };
+  } );
 
-    // Put it on the thing~!~~!!!
+  x.domain( d3.extent( data, function ( d ) {
+    return d.date;
+  } ) );
 
-    // var oil = color2.domain().map(function(name) {
-    //   // statesGlobal.push(name)
-    //   return {
-    //     name: name,
-    //     values: barrels.map(function(d) {
+  svg.append( "g" )
+    .attr( "class", "x axis" )
+    .attr( "transform", "translate(0," + height + ")" )
+    .call( xAxis );
 
-    //       var f = +d[name];          
-    //       return {
-    //         date: d.date,
-    //         barrels: f
-    //       };
-    //     })
-    //   };
-    // });
+  // calculate Y axis min/max from combo array
+  y.domain( [ d3.min( comboValues ), d3.max( comboValues ) / 10 ] );
 
-    var cities = color.domain().map(function(name) {
-    	statesGlobal.push(name)
-      return {
-        name: name,
-        values: data.map(function(d) {
+  svg.append( "g" )
+    .attr( "class", "y axis" )
+    .call( yAxis )
+    .append( "text" )
+    .attr( "transform", "rotate(-90)" )
+    .attr( "x", -height )
+    .attr( "y", 6 )
+    .attr( "dy", ".71em" )
+    .style( "text-anchor", "start" )
+    .text( "CO2 Emissions Change (Million Tonnes)" );
 
-        	var f = +d[name];
-        	
-        	// Loops and adds in other intensities!
-        	for (var k in d){
-        		if (k.slice(0,-2) == name && k.slice(-2) == 01 ) {
-        			var g = +d[k]
-        		} 
-        	}
-          return {
-          	date: d.date,
-           	co2: f,
-           	pcc: g         };
-        })
-      };
-    });
+  var country = svg.selectAll( ".country" )
+    .data( countries )
+    .enter().append( "g" )
+    .attr( "class", "country" );
 
-    x.domain(d3.extent(data, function(d) { return d.date; }));
+  country.append( "path" )
+    .attr( "class", "line" )
+    .attr( "d", function ( d ) {
+      var line = d3.svg.line()
+        .interpolate( "basis" )
+        .x( function ( d ) {
+          return x( d.date );
+        } )
+        .y( function ( d ) {
+          return y( d[ "co2" ] );
+        } )
+      return line( d.values )
+    } )
+    .attr( "attribute", function ( d ) {
+      return d.name
+    } )
+    .attr( "tabID", "co2" )
+    .style( "stroke", function ( d, i ) {
+      var color = "#111"
+      return color;
+    } )
+    .style( "stroke-opacity", "0.10" )
+    .style( "stroke-linecap", "round" )
+    .on( "mouseover", function ( d ) {
+      Highlight( d.name )
+    } )
+    .on( "mouseout", function ( d ) {
+      // d3.select(this).style("stroke-opacity","0.15")
+    } );
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
+  d3.select( '[attribute="United States"]' )
+    .style( "stroke-width", "5" )
+    .style( "stroke-opacity", "0.6" )
+    .style( "stroke", "rgb(139,204,0)" )
 
-  // put this in the update
-    y.domain([
-      -550,300
-    ]);
-
-    // y2.domain([
-    //   -70,39
-    //   ])
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -height)
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "start")
-        .text("CO2 Emissions Change (Million Tonnes)");
-
-    // svg.append("g")
-    //     .attr("class", "y2 axis")
-    //     .attr("transform", "translate(" + (width-20) + " ,0)") 
-    //     .call(yAxis2)
-    //   .append("text")
-    //     .attr("transform", "rotate(-90)")
-    //     .attr("x", -height)
-    //     .attr("y", -15)
-    //     .attr("dy", ".71em")
-    //     .style("text-anchor", "start")
-    //     .text("% Change Net Imports of Crude Oil");
-
-    var city = svg.selectAll(".city")
-        .data(cities)
-      .enter().append("g")
-        .attr("class", "city");  
-
-    // var barrel = svg.selectAll(".barrel")
-    //     .data(oil)
-    //   .enter().append("g")
-    //     .attr("class","barrel")
-
-    // barrel.append("path")
-    //   .attr("class","line2")
-    //   .attr("d",function(d){
-    //     var line2 = d3.svg.line()
-    //       .interpolate("basis")
-    //       .x(function(d) { return x(d.date); })
-    //       .y(function(d) { return y2(d["barrels"]); })
-    //       return line2(d.values)
-    //   })
-    //   .attr("attribute",function(d){return d.name})
-    //   .style("stroke", function(d,i) {        
-    //     var color = "rgb(17,114,161)"
-    //     return color;        
-    //   })
-    //   .style("stroke-opacity","1")
-    //   .style("stroke-width","5")
-    //   .style("stroke-linecap","round")
-    //   .on("mouseover",function(d){          
-    //     BarrelHighlight(d.name)       
-    //   })
-    //   .on("mouseout",function(d){
-    //     // d3.select(this).style("stroke-opacity","0.15")
-    //   });
-
-    city.append("path")
-        .attr("class", "line")
-        .attr("d", function(d) { 		
-          var line = d3.svg.line()
-          .interpolate("basis")
-          .x(function(d) { return x(d.date); })
-          .y(function(d) { return y(d["co2"]); })
-          return line(d.values) 
-  	  })
-        .attr("attribute",function(d){return d.name})
-        .attr("tabID","co2")
-        .style("stroke", function(d,i) {       	
-          var color = "#111"
-          return color;        
-        })
-        .style("stroke-opacity","0.10")
-        .style("stroke-linecap","round")
-        .on("mouseover",function(d){        	
-        	Highlight(d.name)       
-        })
-        .on("mouseout",function(d){
-          // d3.select(this).style("stroke-opacity","0.15")
-        });
-
-      d3.select('[attribute="United States"]')
-      	.style("stroke-width","5")
-      	.style("stroke-opacity","0.6")
-        .style("stroke","rgb(139,204,0)")
-
-      svg.append("text")
-      	.attr("class","highlightText")
-      	.attr("x",width-10)
-      	.attr("y","0")
-        .attr("name",function(d){
-          return cities[3].name;
-        })
-      	.text(function(d){
-      		var type = "co2" 
-      		var state = cities[3].name;
-      		var amount = cities[3].values[7][type]    	
-          if (amount > 0) {
-            var plus = "+"
-          } else {
-            var plus = ""
-          };
-      		return state + ": " + plus + amount;
-      	})
-
-      svg.append("text")
-        .attr("class","highlightText2")
-        .attr("x",width-10)
-        .attr("y","35")
-        .text("CO2 Emissions Change (Million Tonnes)")
-
-      svg.append("text")
-        .attr("class","USText")
-        .attr("x",290)
-        .attr("y",height-150)
-        .text("U.S. CO2 reductions ⤴")      
-
-      // svg.append("text")
-      //   .attr("class","USText")
-      //   .attr("x",width/2+120)
-      //   .attr("y",height/2+10)
-      //   .text("U.S. Oil Imports ⤵")
-
-  	d3.selectAll(".tab").on("click", function() {
-  		d3.selectAll(".tab").attr("class","tab");
-  		this.className = "tab active";
-  		d3.select("#about_extend").attr("class","");
-  		d3.select("#about").attr("class","about_tab")		
-  		update(this.id)
-  	});
-
-  	d3.select("#about").on("click", function() {
-  		this.className = "about_tab active"			
-  		$("#about_extend").addClass("active");
-  		$("#about_extend").css("display","block");
-  	});
-
-  	//remove about on click out
-  	$(document).on('click', function(event) {
-  	  if (!$(event.target).closest('#about').length) {
-  	    // d3.select("#about_extend").attr("class","");
-  		d3.select("#about").attr("class","about_tab")
-  		$("#about_extend").css("display","none");
-  	  }
-  	});
-
-  	$('#autocompletez').autocomplete({
-  	    lookup: statesGlobal,
-  	    lookupLimit: 10,
-  	    maxHeight:350,
-  	    showNoSuggestionNotice: true,
-  	    noSuggestionNotice: function(){		    	
-  	    	return "No country found"
-  	    },
-  	    onSelect: function (suggestion) {	    	
-  	   		// CAll function that highlights selected line
-  	 		Highlight(suggestion.value)
-  	    }
-  	});		
-  	pymChild.sendHeight();
-  // });
-});
-	
-
-		function Highlight(sug) {
-
-			// Call d3 THIS highlight, everything else lowlight			
-      d3.selectAll(".line").style("stroke-opacity","0.10")        
-			d3.select('[attribute="United States"]').style("stroke-opacity","0.6")
-      d3.select('[attribute="'+ sug +'"]').style("stroke-opacity","0.8")        				
-			d3.select(".highlightText")
-        .attr("name", sug)        
-				.text(function(d){					
-					var data = d3.select('[attribute="'+ sug +'"]')[0][0].__data__;
-		    		var type = d3.select('[attribute="'+ sug +'"]')[0][0].attributes.tabID.value; 
-		    		var state = sug;
-		    		var amount = data.values[7][type]    	
-		    		if (amount > 0) {
-              var plus = "+"
-            } else {
-              var plus = ""
-            };
-            return state + ": " + plus + amount;
-	    		})
-
-			pymChild.sendHeight();
-		}	
-
-		function update(tabID) {				
-			var w = parseInt(d3.select("#master_container").style("width"))
-
-      d3.selectAll('.y.axis').remove();
-      d3.select(".baseline").remove();
-      d3.selectAll(".USText").remove();
-
-      if (tabID == "co2") {
-        y.domain([
-          -550,300
-        ]);
-        var ctext = "CO2 Emissions Change (Million Tonnes)";
-
+  svg.append( "text" )
+    .attr( "class", "highlightText" )
+    .attr( "x", width - 10 )
+    .attr( "y", "0" )
+    .attr( "name", function ( d ) {
+      return countries[ 3 ].name;
+    } )
+    .text( function ( d ) {
+      var type = "co2"
+      var state = countries[ 3 ].name;
+      var amount = countries[ 3 ].values[ 7 ][ type ]
+      if ( amount > 0 ) {
+        var plus = "+"
       } else {
-        y.domain([
-          -10,6
-        ]);
-        var ctext = "CO2 Emissions Change (Tonnes/Person)";
+        var plus = ""
       };
+      return state + ": " + plus + amount;
+    } )
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("x", -height)
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "start")
-      .text(ctext);
+  svg.append( "text" )
+    .attr( "class", "highlightText2" )
+    .attr( "x", width - 10 )
+    .attr( "y", "35" )
+    .text( "CO2 Emissions Change (Million Tonnes)" )
 
-			var city = svg.selectAll(".city").selectAll("path")
+  svg.append( "text" )
+    .attr( "class", "USText" )
+    .attr( "x", 290 )
+    .attr( "y", height - 150 )
+    .text( "U.S. CO2 reductions ⤴" )
 
-			city.transition()
-			   	.duration(1000)
-				.attr("d", function(d) { 			 		
-					var line = d3.svg.line()
-					    .interpolate("basis")
-					    .x(function(d) { return x(d.date); })
-					    .y(function(d) { return y(d[tabID]); })
-					return line(d.values)
-				})
-				.attr("tabID",tabID)
-			    
+  d3.selectAll( ".tab" ).on( "click", function () {
+    d3.selectAll( ".tab" ).attr( "class", "tab" );
+    this.className = "tab active";
+    d3.select( "#about_extend" ).attr( "class", "" );
+    d3.select( "#about" ).attr( "class", "about_tab" )
+    update( this.id )
+  } );
 
-      d3.select(".highlightText2")        
-        .text(ctext) 
+  d3.select( "#about" ).on( "click", function () {
+    this.className = "about_tab active"
+    $( "#about_extend" ).addClass( "active" );
+    $( "#about_extend" ).css( "display", "block" );
+  } );
 
-// d3.select('[attribute="United States"]')
+  //remove about on click out
+  $( document ).on( 'click', function ( event ) {
+    if ( !$( event.target ).closest( '#about' ).length ) {
+      // d3.select("#about_extend").attr("class","");
+      d3.select( "#about" ).attr( "class", "about_tab" )
+      $( "#about_extend" ).css( "display", "none" );
+    }
+  } );
 
-      d3.select(".highlightText")
-        .text(function(d){  
+  $( '#autocompletez' ).autocomplete( {
+    lookup: statesGlobal,
+    lookupLimit: 10,
+    maxHeight: 350,
+    showNoSuggestionNotice: true,
+    noSuggestionNotice: function () {
+      return "No country found"
+    },
+    onSelect: function ( suggestion ) {
+      // CAll function that highlights selected line
+      Highlight( suggestion.value )
+    }
+  } );
+  pymChild.sendHeight();
+  // });
+} );
 
-            var name = this.attributes.name.value;            
-            var data = d3.select('[attribute="'+ name +'"]')[0][0].__data__;        
-            var amount = data.values[7][tabID]    
-            if (amount > 0) {
-              var plus = "+"
-            } else {
-              var plus = ""
-            };
-            return name + ": " + plus + amount;
-          
-          })        
+function Highlight( sug ) {
 
-			pymChild.sendHeight();
-		}			
+  // Call d3 THIS highlight, everything else lowlight
+  d3.selectAll( ".line" ).style( "stroke-opacity", "0.10" )
+  d3.select( '[attribute="United States"]' ).style( "stroke-opacity", "0.6" )
+  d3.select( '[attribute="' + sug + '"]' ).style( "stroke-opacity", "0.8" )
+  d3.select( ".highlightText" )
+    .attr( "name", sug )
+    .text( function ( d ) {
+      var data = d3.select( '[attribute="' + sug + '"]' )[ 0 ][ 0 ].__data__;
+      var type = d3.select( '[attribute="' + sug + '"]' )[ 0 ][ 0 ].attributes.tabID.value;
+      var state = sug;
+      var amount = data.values[ 7 ][ type ]
+      if ( amount > 0 ) {
+        var plus = "+"
+      } else {
+        var plus = ""
+      };
+      return state + ": " + plus + amount;
+    } )
+
+  pymChild.sendHeight();
+}
+
+function update( tabID ) {
+  var w = parseInt( d3.select( "#master_container" ).style( "width" ) )
+
+  d3.selectAll( '.y.axis' ).remove();
+  d3.select( ".baseline" ).remove();
+  d3.selectAll( ".USText" ).remove();
+
+  if ( tabID == "co2" ) {
+    y.domain( [
+          -550, 300
+        ] );
+    var ctext = "CO2 Emissions Change (Million Tonnes)";
+
+  } else {
+    y.domain( [
+          -10, 6
+        ] );
+    var ctext = "CO2 Emissions Change (Tonnes/Person)";
+  };
+
+  svg.append( "g" )
+    .attr( "class", "y axis" )
+    .call( yAxis )
+    .append( "text" )
+    .attr( "transform", "rotate(-90)" )
+    .attr( "x", -height )
+    .attr( "y", 6 )
+    .attr( "dy", ".71em" )
+    .style( "text-anchor", "start" )
+    .text( ctext );
+
+  var country = svg.selectAll( ".country" ).selectAll( "path" )
+
+  country.transition()
+    .duration( 1000 )
+    .attr( "d", function ( d ) {
+      var line = d3.svg.line()
+        .interpolate( "basis" )
+        .x( function ( d ) {
+          return x( d.date );
+        } )
+        .y( function ( d ) {
+          return y( d[ tabID ] );
+        } )
+      return line( d.values )
+    } )
+    .attr( "tabID", tabID )
+
+  d3.select( ".highlightText2" )
+    .text( ctext )
+
+  // d3.select('[attribute="United States"]')
+
+  d3.select( ".highlightText" )
+    .text( function ( d ) {
+
+      var name = this.attributes.name.value;
+      var data = d3.select( '[attribute="' + name + '"]' )[ 0 ][ 0 ].__data__;
+      var amount = data.values[ 7 ][ tabID ]
+      if ( amount > 0 ) {
+        var plus = "+"
+      } else {
+        var plus = ""
+      };
+      return name + ": " + plus + amount;
+
+    } )
+
+  pymChild.sendHeight();
+}
 
 // Prebaked functions
-function randomIntFromInterval(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}		
+function randomIntFromInterval( min, max ) {
+  return Math.floor( Math.random() * ( max - min + 1 ) + min );
+}
